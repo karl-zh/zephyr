9b7a37bd7b8f61531e61ae9d394bc70831f31f51
Andy Gross <andy.gross@linaro.org>
2018-08-21 00:08:12 -0500

    frdm_k64f: Add UART1 support
    Signed-off-by: Andy Gross <andy.gross@linaro.org>

--- boards/arm/frdm_k64f/pinmux.c
+++ boards/arm/frdm_k64f/pinmux.c
@@ -39,6 +39,21 @@ static int frdm_k64f_pinmux_init(struct device *dev)
 	pinmux_pin_set(portb, 17, PORT_PCR_MUX(kPORT_MuxAlt3));
 #endif
 
+#ifdef CONFIG_UART_MCUX_1
+	/* UART1 RX, TX */
+	pinmux_pin_set(portb, 18, PORT_PCR_MUX(kPORT_MuxAsGpio));
+	pinmux_pin_set(portc, 3, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portc, 4, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
+#ifdef CONFIG_UART_MCUX_2
+	/* UART2 RX, TX */
+	pinmux_pin_set(portd, 0, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portd, 1, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portd, 2, PORT_PCR_MUX(kPORT_MuxAlt3));
+	pinmux_pin_set(portd, 3, PORT_PCR_MUX(kPORT_MuxAlt3));
+#endif
+
 #ifdef CONFIG_UART_MCUX_3
 	/* UART3 RX, TX */
 	pinmux_pin_set(portc, 16, PORT_PCR_MUX(kPORT_MuxAlt3));
